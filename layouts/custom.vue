173f2ebc087e958d812dcaff8d972ca7e134c004
Jan Hromádka <jan.hromadka@ydealsoft.com>
2022-06-05 10:52:57 +0200

    updated header idea

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,12 +1,81 @@
 <template>
   <div class="w-full h-full bg-blue-100">
-    <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-      <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <div class="bg-purple-800 p-6">
+      <div class="bg-white flex flex-col font-sans">
+        <div class="container mx-auto px-8">
+          <header
+            class="flex flex-col sm:flex-row items-center justify-between py-6 relative"
+          >
+            <h3 class="text-2xl font-bold uppercase text-blue-900">
+              TZB - Martin KOPECKÝ
+            </h3>
+            <nav class="hidden md:flex text-lg">
+              <a
+                href="#"
+                class="text-gray-800 hover:text-purple-300 py-3 px-6"
+              >Domů</a>
+              <a
+                href="#"
+                class="text-gray-800 hover:text-purple-300 py-3 px-6"
+              >Služby</a>
+              <a
+                href="#"
+                class="text-gray-800 hover:text-purple-300 py-3 px-6"
+              >O nás</a>
+              <a
+                href="#"
+                class="text-gray-800 hover:text-purple-300 py-3 px-6"
+              >Kontakt</a>
+              <a
+                href="#"
+                class="text-gray-800 hover:text-purple-300 py-3 px-6"
+              >FAQ</a>
+            </nav>
+            <button
+              class="flex md:hidden flex-col absolute top-0 right-0 p-4 mt-5"
+            >
+              <span class="w-5 h-px mb-1 bg-orange-500" />
+              <span class="w-5 h-px mb-1 bg-orange-500" />
+              <span class="w-5 h-px mb-1 bg-orange-500" />
+            </button>
+          </header>
+          <main
+            class="flex flex-col-reverse sm:flex-row jusitfy-between items-center py-12"
+          >
+            <div
+              class="sm:w-1/2 flex flex-col items-center sm:items-start text-center sm:text-left"
+            >
+              <h1
+                class="uppercase text-4xl text-blue-900 font-bold leading-none tracking-wide mb-2"
+              >
+                Technické zařízení budov
+              </h1>
+              <h2
+                class="uppercase text-2xl text-orange-500 text-secondary tracking-widest mb-6"
+              >
+                Martin Kopecký
+              </h2>
+              <p class="text-gray-600 leading-relaxed mb-12">
+                Projekt se zabývá projektovými prácemi v oblasti stavebnictví se zaměřením na vytápění, rozvod plynu a kanalizace.
+                Zpracováváme kompletní soubor projektů TZB včetně průkazu energetické náročnosti budovy PENB.
+              </p>
+              <a
+                href="#"
+                class="bg-purple-300 hover:bg-purple-400 py-3 px-6 uppercase text-lg font-bold text-white rounded-full"
+              >Learn more</a>
+            </div>
+            <div class="mb-16 sm:mb-0 mt-8 sm:mt-0 sm:w-1/2 sm:pl-12" />
+          </main>
+        </div>
+      </div>
+    </div>
+    <!-- <nav class="flex items-center justify-between flex-wrap p-6">
+      <div class="flex items-center flex-shrink-0  mr-6">
         <span class="font-semibold text-xl tracking-tight">TZB PROJEKT KOPECKÝ</span>
       </div>
       <div class="block lg:hidden">
         <button
-          class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+          class="flex items-center px-3 py-2 border rounded text-teal-200 hover:border-white"
         >
           <svg
             class="fill-current h-3 w-3"
@@ -19,43 +88,42 @@
         </button>
       </div>
       <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-        </div>
+        <div class="text-sm lg:flex-grow" />
         <div>
           <a
             href="#responsive-header"
-            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-800"
           >
             Home
           </a>
           <a
             href="#responsive-header"
-            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-800"
           >
             Služby
           </a>
           <a
             href="#responsive-header"
-            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-800"
           >
             Reference
           </a>
-              <a
-                href="#responsive-header"
-                class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-              >
-                    Kontakt
-                  </a>
-                </div>
-              </div>
-            </nav>
-        <Nuxt />
+          <a
+            href="#responsive-header"
+            class="block mt-4 mr-6 uppercase lg:inline-block lg:mt-0 text-teal-200 hover:text-blue-800"
+          >
+            Kontakt
+          </a>
+        </div>
       </div>
-    </template>
-    <script lang="ts">
+    </nav> -->
+    <Nuxt />
+  </div>
+</template>
+<script lang="ts">
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+  name: 'IndexPage'
 })
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  content: [],
   theme: {
     colors: {
       blue: {
-        100: '#DFEAFE'
+        100: '#DFEAFE',
+        200: '#C0D4FD',
+        300: '#9FBAF9',
+        400: '#86A4F3',
+        500: '#5F82EB',
+        600: '#4563CA',
+        700: '#2F47A9',
+        800: '#1E3088',
+        900: '#122070'
       },
+      white: '#ffffff'
     },
     fontFamily: {
       sans: ['Titillium Web'],
@@ -16,69 +24,63 @@ module.exports = {
     extend: {
       spacing: {
         '8xl': '96rem',
-        '9xl': '128rem',
+        '9xl': '128rem'
       },
       borderRadius: {
-        '4xl': '2rem',
+        '4xl': '2rem'
       }
     }
   },
   plugins: [],
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
-  ],
-
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ]
 
 }
 
 // {
-//   "color-primary-100": "#DFEAFE",
-//   "color-primary-200": "#C0D4FD",
-//   "color-primary-300": "#9FBAF9",
-//   "color-primary-400": "#86A4F3",
-//   "color-primary-500": "#5F82EB",
-//   "color-primary-600": "#4563CA",
-//   "color-primary-700": "#2F47A9",
-//   "color-primary-800": "#1E3088",
-//   "color-primary-900": "#122070",
-//   "color-success-100": "#F2FBCE",
-//   "color-success-200": "#E3F89F",
-//   "color-success-300": "#C8EC6C",
-//   "color-success-400": "#ABD946",
-//   "color-success-500": "#84C113",
-//   "color-success-600": "#6BA50D",
-//   "color-success-700": "#548A09",
-//   "color-success-800": "#3F6F06",
-//   "color-success-900": "#305C03",
-//   "color-info-100": "#D5FEFA",
-//   "color-info-200": "#ACFDFB",
-//   "color-info-300": "#82F4FB",
-//   "color-info-400": "#62E4F7",
-//   "color-info-500": "#30CBF2",
-//   "color-info-600": "#23A0D0",
-//   "color-info-700": "#1879AE",
-//   "color-info-800": "#0F578C",
-//   "color-info-900": "#093E74",
-//   "color-warning-100": "#FEF7CE",
-//   "color-warning-200": "#FEEE9E",
-//   "color-warning-300": "#FEE16E",
-//   "color-warning-400": "#FDD54A",
-//   "color-warning-500": "#FCC10F",
-//   "color-warning-600": "#D8A00A",
-//   "color-warning-700": "#B58107",
-//   "color-warning-800": "#926404",
-//   "color-warning-900": "#784F02",
-//   "color-danger-100": "#FFE8D6",
-//   "color-danger-200": "#FFCCAD",
-//   "color-danger-300": "#FFA983",
-//   "color-danger-400": "#FF8865",
-//   "color-danger-500": "#FF5132",
-//   "color-danger-600": "#DB3124",
-//   "color-danger-700": "#B71919",
-//   "color-danger-800": "#930F1A",
-//   "color-danger-900": "#7A091B"
+
+// "color-primary-700": "#2F47A9",
+// "color-primary-800": "#1E3088",
+// "color-primary-900": "#122070",
+// "color-success-100": "#F2FBCE",
+// "color-success-200": "#E3F89F",
+// "color-success-300": "#C8EC6C",
+// "color-success-400": "#ABD946",
+// "color-success-500": "#84C113",
+// "color-success-600": "#6BA50D",
+// "color-success-700": "#548A09",
+// "color-success-800": "#3F6F06",
+// "color-success-900": "#305C03",
+// "color-info-100": "#D5FEFA",
+// "color-info-200": "#ACFDFB",
+// "color-info-300": "#82F4FB",
+// "color-info-400": "#62E4F7",
+// "color-info-500": "#30CBF2",
+// "color-info-600": "#23A0D0",
+// "color-info-700": "#1879AE",
+// "color-info-800": "#0F578C",
+// "color-info-900": "#093E74",
+// "color-warning-100": "#FEF7CE",
+// "color-warning-200": "#FEEE9E",
+// "color-warning-300": "#FEE16E",
+// "color-warning-400": "#FDD54A",
+// "color-warning-500": "#FCC10F",
+// "color-warning-600": "#D8A00A",
+// "color-warning-700": "#B58107",
+// "color-warning-800": "#926404",
+// "color-warning-900": "#784F02",
+// "color-danger-100": "#FFE8D6",
+// "color-danger-200": "#FFCCAD",
+// "color-danger-300": "#FFA983",
+// "color-danger-400": "#FF8865",
+// "color-danger-500": "#FF5132",
+// "color-danger-600": "#DB3124",
+// "color-danger-700": "#B71919",
+// "color-danger-800": "#930F1A",
+// "color-danger-900": "#7A091B"
 // }
