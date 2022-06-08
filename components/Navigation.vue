aa1ded0b93fec98c06ca037a14506aea2c112dfd
Jan Hromádka <jan.hromadka@ydealsoft.com>
2022-06-08 20:40:28 +0200

    Basic layout of landing page

---
+++ components/Navigation.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav
+    class=" container mx-auto px-8 flex flex-col sm:flex-row items-center justify-between py-6 relative"
+  >
+    <h3 class="text-2xl font-bold uppercase text-blue-900">
+      TZB - Martin KOPECKÝ
+    </h3>
+    <div class="hidden md:flex text-lg">
+      <a
+        href="#"
+        class="text-gray-800 hover:text-purple-300 py-3 px-6"
+      >
+        Domů
+      </a>
+      <a
+        href="#"
+        class="text-gray-800 hover:text-purple-300 py-3 px-6"
+      >
+        Služby
+      </a>
+      <a
+        href="#"
+        class="text-gray-800 hover:text-purple-300 py-3 px-6"
+      >
+        O nás
+      </a>
+      <a
+        href="#"
+        class="text-gray-800 hover:text-purple-300 py-3 px-6"
+      >
+        Kontakt
+      </a>
+      <a
+        href="#"
+        class="text-gray-800 hover:text-purple-300 py-3 px-6"
+      >
+        FAQ
+      </a>
+    </div>
+    <button
+      class="flex md:hidden flex-col absolute top-0 right-0 p-4 mt-5"
+    >
+      <span class="w-5 h-px mb-1 bg-orange-500" />
+      <span class="w-5 h-px mb-1 bg-orange-500" />
+      <span class="w-5 h-px mb-1 bg-orange-500" />
+    </button>
+  </nav>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,37 @@
 <template>
-  <div />
+  <div class="container mx-auto px-8 bg-white flex flex-col font-sans min-h-screen">
+    <header
+      class="flex flex-col-reverse sm:flex-row jusitfy-between items-center py-12 min-h-screen"
+    >
+      <div
+        class="sm:w-1/2 flex flex-col items-center sm:items-start text-center sm:text-left"
+      >
+        <h1
+          class="uppercase text-4xl text-blue-900 font-bold leading-none tracking-wide mb-2"
+        >
+          Technické zařízení budov
+        </h1>
+        <h2
+          class="uppercase text-2xl text-orange-500 text-secondary tracking-widest mb-6"
+        >
+          Martin Kopecký
+        </h2>
+        <p class="text-gray-600 leading-relaxed mb-12">
+          Projekt se zabývá projektovými prácemi v oblasti stavebnictví se
+          zaměřením na vytápění, rozvod plynu a kanalizace. Zpracováváme
+          kompletní soubor projektů TZB včetně průkazu energetické
+          náročnosti budovy PENB.
+        </p>
+        <a
+          href="#"
+          class="bg-purple-300 hover:bg-purple-400 py-3 px-6 uppercase text-lg font-bold text-white rounded-full"
+        >
+          Learn more
+        </a>
+      </div>
+      <div class="mb-16 sm:mb-0 mt-8 sm:mt-0 sm:w-1/2 sm:pl-12" />
+    </header>
+  </div>
 </template>
 
 <script lang="ts">
